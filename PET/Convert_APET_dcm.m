dad628d25ff037e87867cc16428a608f2d024c3f
sblee9126 <subin9126@hotmail.com>
2021-10-21 16:23:29 +0900

    update pet dicom convert

--- PET/Convert_APET_dcm.m
+++ PET/Convert_APET_dcm.m
@@ -1,12 +1,30 @@
 
-folders = dir('Z:\Personal_Folder\Subin\KUH\APET\DICOM\HY_PET data'); 
+% Code for converting dicom files using SPM12.
+
+%++++++++Specify accordingly++++++++++++++++++++++++++++++++++++++++++++++++++++
+% Parent directory of folders that contain dicom files for each subject
+main_dir = 'Z:\Personal_Folder\Subin\KUH\APET\DICOM\HY_PET data';
+
+% Voxel resolution of converted files
+voxelSize = [1.0157 1.0157 2];
+
+% Image modality that is being converted (included at filename)
+mod = 'APET'
+
+% Output directory containing converted, processed nii files
+out_dir = 'Z:\Personal_Folder\Subin\KUH\APET\0_converted_files'
+
+
+
+%++++++++Do not change below++++++++++++++++++++++++++++++++++++++++++++++++++++
+folders = dir(main_dir); 
 folders = folders(3:end);
 numsubjects = length(folders);
 
 before_after = cell(1,1);
 
 for s = 1:numsubjects
-    subjdir = ['Z:\Personal_Folder\Subin\KUH\APET\DICOM\HY_PET data\' folders(s).name];
+    subjdir = [main_dir '\' folders(s).name];
     
     dir_contents = dir(subjdir);
     dir_contents = dir_contents(3:end);
@@ -30,7 +48,7 @@ for s = 1:numsubjects
     
     % Rename converted files
     output_fname = [subjdir '\' dir(fullfile(subjdir, '*.nii')).name];
-    apet_fname = ['Z:\Personal_Folder\Subin\KUH\APET\0_converted_files\'  folders(s).name '_APET.nii'];
+    apet_fname = [out_dir '\'  folders(s).name '_' mod '.nii'];
     
     before_after{s,1} = output_fname;
     before_after{s,2} = apet_fname;
@@ -38,8 +56,8 @@ for s = 1:numsubjects
     
     % Resave as origin-set version.
     pet = load_untouch_nii(apet_fname);
-%     save_nii(make_nii(pet.img, [2 2 2], [64 64 45]), ['Z:\Personal_Folder\Subin\KUH\APET\1_origin_set_files\'  folders(s).name '_APET.nii'])
-    save_nii(make_nii(pet.img, [1.0157 1.0157 2], [168 168 41]), ['Z:\Personal_Folder\Subin\KUH\APET\1_origin_set_files\'  folders(s).name '_APET.nii'])
+    origLoc = [(size(pet.img,1)/2) (size(pet.img,2)/2) (size(pet.img,3)/2)];
+    save_nii(make_nii(pet.img, voxelSize, origLoc), [out_dir '\'  folders(s).name '_' mod '.nii'])
 
     
 end
